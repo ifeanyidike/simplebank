0e3c049576d8a73b1c503e05040b176f77ad6c47
Ifeanyi Dike <lorddickson751@gmail.com>
2023-11-27 09:49:02 +0100

    update start.sh

--- Dockerfile
+++ Dockerfile
@@ -41,9 +41,8 @@ RUN chmod +x /app/migrate
 
 
 EXPOSE 8081
-# CMD [ "/app/main" ]
-CMD [ "/app/main", "-addr", "$SERVER_ADDRESS" ]
+CMD [ "/app/main" ]
 ENTRYPOINT [ "/app/start.sh" ]
 # CMD ["./wait-for-it.sh", "postgres:5432", "--", "./main", "migrate"]
 # Command to run the entrypoint script
-# CMD ["./entrypoint.sh"]
+# CMD ["./start.sh"]
--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ echo "run db migration"
 # /app/migrate -path /app/db/migration -database "$DB_SOURCE" -verbose up
 ./migrate -path db/migration -database "$DB_SOURCE" -verbose up
 
-echo "start the app"
-exec "$@"
+echo "start the app with SERVER_ADDRESS: $SERVER_ADDRESS"
+exec /app/main -addr="$SERVER_ADDRESS"
+
+# echo "start the app"
+# exec "$@"
 # ./main
