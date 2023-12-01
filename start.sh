5b989418b8b049b9381433762827f6316c72d32d
Ifeanyi Dike <lorddickson751@gmail.com>
2023-12-01 23:42:01 +0100

    modify start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,6 @@ echo "run db migration"
 # /app/migrate -path /app/db/migration -database "$DB_SOURCE" -verbose up
 ./migrate -path db/migration -database "$DB_SOURCE" -verbose up
 
-echo "start the app with SERVER_ADDRESS: $SERVER_ADDRESS"
-exec /app/main -addr="$SERVER_ADDRESS"
-
-# echo "start the app"
-# exec "$@"
+echo "start the app"
+exec "$@"
 # ./main
